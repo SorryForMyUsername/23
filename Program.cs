9bb7974452dec61d2da549ed578d6f4cf234321e
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-02-14 12:50:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ширина: ");
+            int width = int.Parse(Console.ReadLine());
+            Console.Write("Длина: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(width, height);
+            Console.WriteLine(rectangle);
+
+            Console.WriteLine($"Периметр: {rectangle.Perimeter()}");
+            Console.WriteLine($"Площадь: {rectangle.Square()}");
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_2
+{
+    internal class Rectangle
+    {
+        /// <summary>
+        /// Ширина
+        /// </summary>
+        private int width;
+        /// <summary>
+        /// Длина
+        /// </summary>
+        private int length;
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Rectangle() { }
+        /// <summary>
+        /// Конструктор с одним параметром
+        /// </summary>
+        /// <param name="width">Ширина</param>
+        public Rectangle(int width) { 
+            this.Width = width;
+        }
+        /// <summary>
+        /// Конструктор с двумя параметрами
+        /// </summary>
+        /// <param name="width">Ширина</param>
+        /// <param name="length">Длина</param>
+        public Rectangle(int width, int length) : this(width)
+        {
+            this.Length = length; 
+        }
+
+        /// <summary>
+        /// Длина
+        /// </summary>
+        public int Length { get => length; 
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Значение меньше нуля!");
+                }
+                this.length = value;
+            }
+        }
+        /// <summary>
+        /// Ширина
+        /// </summary>
+        public int Width { get => width;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Значение меньше нуля!");
+                }
+                this.width = value;
+            }
+        }
+
+        /// <summary>
+        /// Метод, выводящий информацию об объекте.
+        /// </summary>
+        /// <returns>Информацию об объекте в виде строки</returns>
+        public override string ToString()
+        {
+            return $"Ширина: {Width}\tДлина: {Length}";
+        }
+
+        /// <summary>
+        /// Метод, вычисляющий периметр
+        /// </summary>
+        /// <returns>Периметр прямоугольника</returns>
+        public int Perimeter()
+        {
+            return (Width + Length) * 2;
+        }
+
+        /// <summary>
+        /// Метод, вычисляющий площадь
+        /// </summary>
+        /// <returns>Площадь прямоугольника</returns>
+        public int Square()
+        {
+            return Width * Length;
+        }
+    }
+}
