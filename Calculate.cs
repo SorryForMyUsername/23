ea01be4a493deca62cfbb67f79e36eaf1be1e6d7
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-02-14 12:13:26 +0200

    Add files via upload

---
+++ Calculate.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Класс, описывающий вычисления
+    /// </summary>
+    internal class Calculate
+    {
+        /// <summary>
+        /// Поле 1
+        /// </summary>
+        private double field_1;
+
+        /// <summary>
+        /// Поле 2
+        /// </summary>
+        private double field_2;
+        public double Field_1 { get => field_1; 
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("Значение меньше нуля!");
+                }
+                field_1 = value;
+            } 
+        }
+        public double Field_2 { get => field_2;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Значение меньше нуля!");
+                }
+                field_2 = value;
+            }
+        }
+
+        /// <summary>
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public Calculate() { }
+        
+        /// <summary>
+        /// Конструктор с одним параметром.
+        /// </summary>
+        /// <param name="field_1">Поле 1</param>
+        public Calculate(double field_1) 
+        {  
+            this.Field_1 = field_1; 
+        } 
+
+        /// <summary>
+        /// Конструктор с двумя параметрами.
+        /// </summary>
+        /// <param name="field_1">Поле 1</param>
+        /// <param name="field_2">Поле 2</param>
+        public Calculate(double field_1, double field_2) : this(field_1)
+        {
+            this.Field_2 = field_2;
+        }
+
+        /// <summary>
+        /// функция формирования строки с информацией об объекте.
+        /// </summary>
+        /// <returns>информацию об объекте в виде строки.</returns>
+        public override string ToString()
+        {
+            return $"Поле 1: {Field_1}\tПоле 2: {Field_2}";
+        }
+
+        /// <summary>
+        /// Функция формирования строки с информацией об объекте.
+        /// </summary>
+        public void Output()
+        {
+            Console.WriteLine($"Поле 1: {Field_1}\tПоле 2: {Field_2}");
+        }
+
+        /// <summary>
+        /// функция обработки значений полей.
+        /// </summary>
+        /// <returns>Корень квадратный из произведения чисел.</returns>
+        public double Handle()
+        {
+            return Math.Sqrt(Field_1 * Field_2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calc1 = new Calculate();
+            calc1.Output();
+            Console.WriteLine(calc1);
+            calc1.Field_1 = 3;
+            calc1.Field_2 = 10;
+            calc1.Output();
+            Console.WriteLine();
+
+            Calculate calc2 = new Calculate(4);
+            calc2.Output();
+            Console.WriteLine();
+
+            Calculate calc3 = new Calculate(2, 3);
+            calc3.Output();
+            Console.WriteLine($"Результат обработки: {calc3.Handle():f2}\n");
+
+            Console.Write("Поле 1: ");
+            double field_1 = double.Parse(Console.ReadLine());
+            Console.Write("Поле 2: ");
+            double field_2 = double.Parse(Console.ReadLine());
+            Calculate calc4 = new Calculate(field_1, field_2);
+            calc4.Output();
+            Console.WriteLine($"Результат обработки: {calc4.Handle():f2}");
+
+            Console.ReadKey(true);
+        }
+    }
+}
